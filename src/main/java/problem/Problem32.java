ca74c5aa91ec514c4e08d4cc4b86e09a7549deab
wooheaven <wooheaven79@gmail.com>
2016-01-11 21:41:24 +0900

    PandigitalNumber is finished

--- src/main/java/problem/Problem32.java
+++ src/main/java/problem/Problem32.java
@@ -6,46 +6,64 @@ import java.util.List;
 public class Problem32 {
 
 	public static void main(String[] args) {
-		MultiplicandList mcList = new MultiplicandList(1);
+		PandigitalNumber mcList = new PandigitalNumber(1);
 		mcList.subListOfPowerSet();
 		System.out.println(mcList.toString());
 
-		mcList = new MultiplicandList(2);
+		mcList = new PandigitalNumber(2);
+		mcList.subListOfPowerSet();
+		System.out.println(mcList.toString());
+		
+		mcList = new PandigitalNumber(3);
 		mcList.subListOfPowerSet();
 		System.out.println(mcList.toString());
 	}
 }
 
-class MultiplicandList {
+class PandigitalNumber
+{
 	private int digitCount;
-	private List<Integer> multiplicandList;
-	private int[] numberList = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	private List<Integer> pandigitalNumberList;
+	private int[] initialNumber = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	public MultiplicandList(int count) {
+	public PandigitalNumber(int count) {
 		this.digitCount = count;
-		multiplicandList = new ArrayList<Integer>();
+		pandigitalNumberList = new ArrayList<Integer>();
 	}
 
 	public void subListOfPowerSet() {
-		while (1 <= digitCount) {
-			if (0 == multiplicandList.size()) {
-				for (int i = 0; i < numberList.length; i++) {
-					multiplicandList.add(numberList[i]);
+		while (0 < digitCount) {
+			if (0 == pandigitalNumberList.size()) {
+				for (int i = 0; i < initialNumber.length; i++) {
+					pandigitalNumberList.add(initialNumber[i]);
 				}
 			} else {
 				List<Integer> tempMultiplicandList = new ArrayList<Integer>();
-				for (int i = 0; i < multiplicandList.size(); i++) {
-					for (int j = 0; j < numberList.length; j++) {
-						if (multiplicandList.get(i) != numberList[j]) {
-							int tempNum = multiplicandList.get(i) * 10 + numberList[j];
+				for (int i = 0; i < pandigitalNumberList.size(); i++) {
+					for (int j = 0; j < initialNumber.length; j++) {
+						if (isNumberUnique(pandigitalNumberList.get(i), initialNumber[j])) {
+							int tempNum = pandigitalNumberList.get(i) * 10 + initialNumber[j];
 							tempMultiplicandList.add(tempNum);
 						}
 					}
 				}
-				multiplicandList = tempMultiplicandList;
+				pandigitalNumberList = tempMultiplicandList;
 			}
 			digitCount--;
 		}
+	}
+
+	private boolean isNumberUnique(Integer oldInt, int newInt) {
+		boolean result = true;
+		int temp = 0;
+		while (oldInt > 0) {
+			temp = oldInt % 10;
+			if (temp == newInt) {
+				result = false;
+			}
+			oldInt = (oldInt - temp) / 10;
+		}
+		return result;
 	}
 
 	public int getDigitCount() {
@@ -57,25 +75,25 @@ class MultiplicandList {
 	}
 
 	public List<Integer> getMultiplicandList() {
-		return multiplicandList;
+		return pandigitalNumberList;
 	}
 
 	public void setMultiplicandList(List<Integer> multiplicandList) {
-		this.multiplicandList = multiplicandList;
+		this.pandigitalNumberList = multiplicandList;
 	}
 
 	public int[] getNumberList() {
-		return numberList;
+		return initialNumber;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("MultiplicandList\n");
-		for (int i = 0; i < multiplicandList.size(); i++) {
-			sb.append(i+1 + "번째 원소");
+		for (int i = 0; i < pandigitalNumberList.size(); i++) {
+			sb.append(i + 1 + "번째 원소");
 			sb.append("\t=\t");
-			sb.append(multiplicandList.get(i));
+			sb.append(pandigitalNumberList.get(i));
 			sb.append("\n");
 		}
 		return sb.toString();
