c70a7a71fc826fdfcd0127ba7cadd5e6daa9e8fb
wooheaven <wooheaven79@gmail.com>
2016-10-03 21:12:01 +0900

    Problem32 with TDD

---
+++ src/test/java/problem/Problem32Test.java
@@ -0,0 +1,14 @@
+package problem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Problem32Test {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
