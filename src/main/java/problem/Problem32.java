1cec8b96da0deb0491ca8d10cc7f786de885e263
wooheaven <wooheaven79@gmail.com>
2016-10-03 21:44:53 +0900

    1st : Make it pass

--- src/main/java/problem/Problem32.java
+++ src/main/java/problem/Problem32.java
@@ -1,5 +1,23 @@
 package problem;
 
+import java.util.HashSet;
+
 
 public class Problem32 {
+
+    public boolean isPendigital(int i) {
+        int digitCount = String.valueOf(i).length();
+        
+        HashSet<Integer> digitNumbers = new HashSet<Integer>();
+        while(i%10 > 0){
+            digitNumbers.add(i%10);
+            i = (i - i%10)/10;
+        }
+        
+        if(digitCount==digitNumbers.size()){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/problem/Problem32Test.java
+++ src/test/java/problem/Problem32Test.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class Problem32Test {
 
     @Test
-    public void test() {
-        Problem32 p32 = new Problem32Test();
+    public void test15234_to_true() {
+        Problem32 p32 = new Problem32();
         assertTrue(p32.isPendigital(15234));
     }
 
