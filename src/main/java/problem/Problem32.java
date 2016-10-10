b3e08363bd09616bc91ee32a2a547fa37b24f9aa
rwoo <wooheaven@naver.com>
2016-10-10 22:37:34 +0900

    3rd fail test, make it pass, refactoring

--- src/main/java/problem/Problem32.java
+++ src/main/java/problem/Problem32.java
@@ -4,79 +4,49 @@ import java.util.HashSet;
 
 
 public class Problem32 {
-	public boolean isUniqNumbers(int i) {
-		int[] intArray = new int[1];
-		intArray[0] = i;
-		return isUniqNumbers(intArray);
-	}
+    public boolean isNinePendigital(int i) {
+        int[] intArray = new int[1];
+        intArray[0] = i;
 
-	public boolean isUniqNumbers(int i, int j) {
-		int[] intArray = new int[2];
-		intArray[0] = i;
-		intArray[1] = j;
-		return isUniqNumbers(intArray);
-	}
+        return isNinePendigital(intArray);
+    }
 
-	public boolean isUniqNumbers(int i, int j, int k) {
-		int[] intArray = new int[3];
-		intArray[0] = i;
-		intArray[1] = j;
-		intArray[2] = k;
-		return isUniqNumbers(intArray);
-	}
+    public boolean isNinePendigital(int i, int j) {
+        int[] intArray = new int[2];
+        intArray[0] = i;
+        intArray[1] = j;
 
-	private boolean isUniqNumbers(int[] intArray) {
-		int digitCount = 0;
-		for (int i = 0; i < intArray.length; i++) {
-			digitCount += String.valueOf(intArray[i]).length();
-		}
+        return isNinePendigital(intArray);
+    }
 
-		HashSet<Integer> digitNumbers = new HashSet<Integer>();
-		for (int i = 0; i < intArray.length; i++) {
-			int tmpInt = intArray[i];
-			while (tmpInt % 10 > 0) {
-				digitNumbers.add(tmpInt % 10);
-				tmpInt = (tmpInt - tmpInt % 10) / 10;
-			}
-		}
+    public boolean isNinePendigital(int i, int j, int k) {
+        int[] intArray = new int[3];
+        intArray[0] = i;
+        intArray[1] = j;
+        intArray[2] = k;
 
-		if (digitCount == digitNumbers.size()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        return isNinePendigital(intArray);
+    }
 
-	public boolean isNinePendigital(int i) {
-		int[] intArray = new int[1];
-		intArray[0] = i;
+    private boolean isNinePendigital(int[] intArray) {
+        int digitCount = 0;
+        for (int i = 0; i < intArray.length; i++) {
+            digitCount += String.valueOf(intArray[i]).length();
+        }
 
-		return isUniqNumbers(intArray) && isNinePendigital(intArray);
-	}
+        HashSet<Integer> digitNumbers = new HashSet<Integer>();
+        for (int i = 0; i < intArray.length; i++) {
+            int tmpInt = intArray[i];
+            while (tmpInt % 10 > 0) {
+                digitNumbers.add(tmpInt % 10);
+                tmpInt = (tmpInt - tmpInt % 10) / 10;
+            }
+        }
 
-	public boolean isNinePendigital(int i, int j) {
-		int[] intArray = new int[2];
-		intArray[0] = i;
-		intArray[1] = j;
-
-		return isUniqNumbers(intArray) && isNinePendigital(intArray);
-	}
-
-	public boolean isNinePendigital(int i, int j, int k) {
-		int[] intArray = new int[3];
-		intArray[0] = i;
-		intArray[1] = j;
-		intArray[2] = k;
-
-		return isUniqNumbers(intArray) && isNinePendigital(intArray);
-	}
-
-	private boolean isNinePendigital(int[] intArray) {
-		int digitCount = 0;
-		for (int i = 0; i < intArray.length; i++) {
-			digitCount += String.valueOf(intArray[i]).length();
-		}
-
-		return digitCount == 9;
-	}
+        if(digitCount==digitNumbers.size() && digitCount == 9){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/problem/Problem32Test.java
+++ src/test/java/problem/Problem32Test.java
@@ -3,43 +3,43 @@ package problem;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class Problem32Test {
-	private Problem32 p32;
+    private Problem32 p32;
 
-	@Before
-	public void setup() {
-		p32 = new Problem32();
-	}
+    @Before
+    public void setup() {
+        p32 = new Problem32();
+    }
 
-	@Test
-	public void test_15234_is_uniq() {
-		assertTrue(p32.isUniqNumbers(15234));
-	}
+    @Test
+    public void test_15234_isNot_9pendigital() {
+        assertFalse(p32.isNinePendigital(15234));
+    }
 
-	@Test
-	public void test_15_234_are_uniq() {
-		assertTrue(p32.isUniqNumbers(15, 234));
-	}
+    @Test
+    public void test_15_234_areNot_9pendigital() {
+        assertFalse(p32.isNinePendigital(15, 234));
+    }
 
-	@Test
-	public void test_15_23_4_are_uniq() {
-		assertTrue(p32.isUniqNumbers(15, 23, 4));
-	}
+    @Test
+    public void test_15_23_4_areNot_9pendigital() {
+        assertFalse(p32.isNinePendigital(15, 23, 4));
+    }
 
-	@Test
-	public void test_152346789_is_9pendigital() {
-		assertTrue(p32.isNinePendigital(152346789));
-	}
+    @Test
+    public void test_152346789_is_9pendigital() {
+        assertTrue(p32.isNinePendigital(152346789));
+    }
 
-	@Test
-	public void test_15_2346789_is_9pendigital() {
-		assertTrue(p32.isNinePendigital(15, 2346789));
-	}
+    @Test
+    public void test_15_2346789_is_9pendigital() {
+        assertTrue(p32.isNinePendigital(15, 2346789));
+    }
 
-	@Test
-	public void test_15_23_46789_is_9pendigital() {
-		assertTrue(p32.isNinePendigital(15, 23, 46789));
-	}
+    @Test
+    public void test_15_23_46789_is_9pendigital() {
+        assertTrue(p32.isNinePendigital(15, 23, 46789));
+    }
 }
